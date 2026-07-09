b863d136bd84ad0e5011be4eea8518f7b2a0f717
Dewansh Gupta <dewanshgupta23@gmail.com>
2026-07-09 21:33:47 +0530

    Refactor code and improve readability

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,179 +1,132 @@
+"""
+Tic-Tac-Toe Game - Python
+"""
+
+
+
+# GAME BOARD 
+
 main_board = [" "]*9
 
-dic = dict(q=0,w=1,e=2,a=3,s=4,d=5,z=6,x=7,c=8)
+positions = dict(q=0,w=1,e=2,a=3,s=4,d=5,z=6,x=7,c=8)
 
 
 # DISPLAY TIC TAC TOE BOARD 
 
 def display_board():
-    print(main_board[dic["q"]] + "  | " + main_board[dic["w"]] + " |  " + main_board[dic["e"]])
+    print(main_board[positions["q"]] + "  | " + main_board[positions["w"]] + " |  " + main_board[positions["e"]])
     print("---|---|---")
-    print(main_board[dic["a"]] + "  | " + main_board[dic["s"]] + " |  " + main_board[dic["d"]])
+    print(main_board[positions["a"]] + "  | " + main_board[positions["s"]] + " |  " + main_board[positions["d"]])
     print("---|---|---")
-    print(main_board[dic["z"]] + "  | " + main_board[dic["x"]] + " |  " + main_board[dic["c"]])
+    print(main_board[positions["z"]] + "  | " + main_board[positions["x"]] + " |  " + main_board[positions["c"]])
 
 
 
-# PLAYER CHOICE PART
+# PLAYER SYMBOL SELECTION
 
-def player_choice():
-    player1 = " " 
-    player2 = " " 
+def player_symbol():
+    player1 = "" 
+    player2 = "" 
 
-    # while player1 != "X" or player1 != "O":         # W R O N G(this will always give false so loop will never end)
     while player1 not in ("X","O"):
-        print("")
+        print()
         player1 = input("Hey! Player 1, choose your symbol ( X or O ) : ").upper()
 
         
         if player1 == "X":
            player2 = "O"
-           print("")
+           print()
            print("Player 2, your symbol is O")
            return player1, player2 
         elif player1 == "O":
            player2 = "X"
-           print("")
+           print()
            print("Player 2, your symbol is X")
            return player1, player2
         else:
-            print("")
+            print()
             print("Sorry, that's an invalid choice. \nPlease enter 'X' or 'O'.")
 
 
 
 
 
-# WIN CHECK PART 
+# CHECK WINNING COMBINATIONS
 
-def win_checks(current_player):
+def win_check(current_player):
 
+    # All winning positions
+    wins = ((0, 1, 2), (3, 4, 5), (6, 7, 8),(0, 3, 6), (1, 4, 7), (2, 5, 8),(0, 4, 8), (2, 4, 6))
 
-    # Horizontal
-
-    if (main_board[dic["q"]] == current_player and
-        main_board[dic["w"]] == current_player and
-        main_board[dic["e"]] == current_player):
-        print(f"Player {current_player} has won this match.")
-        return True
-
-    if (main_board[dic["a"]] == current_player and
-        main_board[dic["s"]] == current_player and
-        main_board[dic["d"]] == current_player):
-        print(f"Player {current_player} has won this match.")
-        return True
-
-    if (main_board[dic["z"]] == current_player and
-        main_board[dic["x"]] == current_player and
-        main_board[dic["c"]] == current_player):
-        print(f"Player {current_player} has won this match.")
-        return True
-
-
-    # Vertical
-
-    if (main_board[dic["q"]] == current_player and
-        main_board[dic["a"]] == current_player and
-        main_board[dic["z"]] == current_player):
-        print(f"Player {current_player} has won this match.")
-        return True
-
-    if (main_board[dic["w"]] == current_player and
-        main_board[dic["s"]] == current_player and
-        main_board[dic["x"]] == current_player):
-        print(f"Player {current_player} has won this match.")
-        return True
-
-    if (main_board[dic["e"]] == current_player and
-        main_board[dic["d"]] == current_player and
-        main_board[dic["c"]] == current_player):
-        print(f"Player {current_player} has won this match.")
-        return True
-
-
-    # Diagonal
-
-    if (main_board[dic["q"]] == current_player and
-        main_board[dic["s"]] == current_player and
-        main_board[dic["c"]] == current_player):
-        print(f"Player {current_player} has won this match.")
-        return True
-
-    if (main_board[dic["e"]] == current_player and
-        main_board[dic["s"]] == current_player and
-        main_board[dic["z"]] == current_player):
-        print(f"Player {current_player} has won this match.")
-        return True
+    for a,b,c in wins:
+        if main_board[a] == main_board[b] == main_board[c] == current_player:
+             print(f"Player {current_player} has won this match.")
+             return True
 
     return False
+        
 
 
+# HANDLE PLAYER MOVES
 
-
-
-# POSITION CHOICE PART
-
-
-def position_choice(players):
+def play_match(players):
     
     player1, player2 = players
     current_player = player1
-    board = main_board
-    # print(board,"\n",track)
-    while " " in board:
+
+    while " " in main_board:
             
       if current_player == player1:
 
-            print("") 
+            print() 
             position_input = input(f"Player 1 ({player1}), choose a position (Q, W, E, A, S, D, Z, X, C): ").lower()     
-            print("") 
-            if position_input not in dic:
+            print() 
+            if position_input not in positions:
              print("Invalid position. Please enter one of: Q, W, E, A, S, D, Z, X, or C.")
              continue 
           
-            if board[dic[position_input]] == " ":
-             board[dic[position_input]] = player1
+            if main_board[positions[position_input]] == " ":
+             main_board[positions[position_input]] = player1
              display_board()
-             if win_checks(player1):
+             if win_check(player1):
                 break
              
              current_player = player2
       
             else:
-             print(f"{board[dic[position_input]]} is already at {position_input.upper()}. Please choose another position.")
+             print(f"{main_board[positions[position_input]]} is already at {position_input.upper()}. Please choose another position.")
 
 
       else:
 
-          print("")
+          print()
           position_input = input(f"Player 2 ({player2}), choose a position (Q, W, E, A, S, D, Z, X, C): ").lower()
-          print("")
-          if position_input not in dic:
+          print()
+          if position_input not in positions:
              print("Invalid position. Please enter one of: Q, W, E, A, S, D, Z, X, C.")
              continue 
           
-          if board[dic[position_input]] == " ":
-             board[dic[position_input]] = player2
+          if main_board[positions[position_input]] == " ":
+             main_board[positions[position_input]] = player2
              display_board()
-             if win_checks(player2):
+             if win_check(player2):
                 break
              current_player = player1
              
           else:
-             print(f"{board[dic[position_input]]} is already at {position_input.upper()}. Please choose another position.")
+             print(f"{main_board[positions[position_input]]} is already at {position_input.upper()}. Please choose another position.")
 
-      # print(board)
-      # print(board.count(" "))
+    
     else:
-       print("")
+       print()
        print("It's a draw, Game over.")
    
 
 
 
-# CLEAR BOARD
+# RESET THE GAME BOARD
 
-def clear_board():
+def reset_board():
     for i in range(len(main_board)):
         main_board[i] = " "
         
@@ -182,19 +135,19 @@ def clear_board():
 
 # ASKING PLAYER TO CONTINUE PLAYING
 
-def keep_playing():
+def play_again():
 
-    ask = " "
+    choice = " "
 
-    while ask not in ("Y", "N"):
-        print("")
-        ask = input("Would you like to play another match? (Y/N): ").upper()
-        print("")
+    while choice not in ("Y", "N"):
+        print()
+        choice = input("Would you like to play another match? (Y/N): ").upper()
+        print()
 
-        if ask == "Y":
-            clear_board()
+        if choice == "Y":
+            reset_board()
             return True
-        elif ask == "N":
+        elif choice == "N":
             return False
         else:
             print("Invalid input. Please enter Y for Yes or N for No.")
@@ -203,33 +156,35 @@ def keep_playing():
 
 
 
-# FINAL PART
-
-main_board = [" "]*9
-
-dic = dict(q=0,w=1,e=2,a=3,s=4,d=5,z=6,x=7,c=8)
+# START THE GAME
 
 play = True
 
-print("")
-print("WELCOME TO THE TIC-TAC-TOE GAME!")
-print("")
+print()
+print("WELCOME TO TIC-TAC-TOE GAME!")
+print()
+
+print("Board Positions:")
+print(" Q | W | E")
+print("-----------")
+print(" A | S | D")
+print("-----------")
+print(" Z | X | C")
+print()
+
 while play:
 
     display_board()
 
-    players = player_choice()
+    players = player_symbol()
 
-    position_choice(players)
+    play_match(players)
     
-    play = keep_playing()
+    play = play_again()
 
 
-print("")
-print("Thanks for playing Tic-Tac-Toe, See you next time!!")
-print("")
+print()
+print("Thanks for playing Tic-Tac-Toe! See you next time!")
+print()
 print(" ~ ~ ")
-print("")
-
-
-
+print()
